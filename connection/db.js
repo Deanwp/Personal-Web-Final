7b04bdf92414f9e62fc53bacf9b82b28c2f81163
unknown <deanwahyu12@gmail.com>
2022-01-21 09:32:35 +0700

    database

--- connection/db.js
+++ connection/db.js
@@ -1,9 +1,10 @@
 const {Pool} = require('pg')
 const dbPool = new Pool({
-    database: 'personal-web-b30',
+    host: 'ec2-107-21-146-133.compute-1.amazonaws.com'
+    database: 'ddv7gd9vkf0j6j',
     port: 5432,
-    user: 'postgres',
-    password: 'user'
+    user: 'sfolraipwbrfcl',
+    password: '82c94a5cfdc1c99fa5c29005e80d76552e0078f4043fc42fb8dbc0a3575e1d17'
 })
 
 module.exports = dbPool
